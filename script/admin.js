8c933ea645fafda76770034314000477f9ce1ae8
yoogukhyeon <rnrgus5897@gmail.com>
2024-08-28 16:27:22 +0900

    feat: admin 작업

--- script/admin.js
+++ script/admin.js
@@ -1,4 +1,6 @@
-document.getElementById('back-button').addEventListener('click', function (event) {
-  event.preventDefault(); // 기본 링크 동작을 막습니다.
-  history.back(); // 이전 페이지로 이동합니다.
+document.addEventListener('DOMContentLoaded', function () {
+  document.getElementById('back-button').addEventListener('click', function (event) {
+    event.preventDefault(); // 기본 링크 동작을 막습니다.
+    history.back(); // 이전 페이지로 이동합니다.
+  });
 });
