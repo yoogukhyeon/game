0d66ed2ba026fac9dd5908fc76d7d12e97a2b550
yoogukhyeon <rnrgus5897@gmail.com>
2024-08-29 09:02:42 +0900

    fix: 뒤로가기 수정

--- script/admin.js
+++ script/admin.js
@@ -1,6 +1,4 @@
-document.addEventListener('DOMContentLoaded', function () {
-  document.getElementById('back-button').addEventListener('click', function (event) {
-    event.preventDefault(); // 기본 링크 동작을 막습니다.
-    history.back(); // 이전 페이지로 이동합니다.
-  });
+document.getElementById('back-button').addEventListener('click', function (event) {
+  event.preventDefault(); // 기본 링크 동작을 막습니다.
+  history.back(); // 이전 페이지로 이동합니다.
 });
